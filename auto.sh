cb19da45405b2ad0d644919c3a17456c761e2316
C T Mccoy <200731565+ctmccoy@users.noreply.github.com>
2025-04-03 23:59:37 -0400

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -6,19 +6,18 @@
 
 echo "Automated Threat Intelligence Information and Analysis"
 
-#Move to project directory
-
+#move to project directory
 cd ~/automated-threat-intelligence || exit 1
 
-#Run Fetch Script
+# running fetch script
 echo "[-] Gathering Threat Intelligence..."
 python3 fetch_feeds.py
 
-#Run Threat Intelligence Fetch Script
+# run analysis script
 echo "[-] Analyzing Threat Intelligence Data..."
 python3 analyze_data.py > output/analysis_report.txt
 
-#Logging
+#log function for management
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 cd output/analysis_report.txt output/analysis_report_$timestamp.txt
 
